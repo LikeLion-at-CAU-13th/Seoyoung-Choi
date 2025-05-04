430cb99e07f385db0f932597502ac5b0dda32581
dallaechoi <choi032799@gmail.com>
2025-05-04 21:02:36 +0900

    comment api CBV 변환

--- likelion13/posts/urls.py
+++ likelion13/posts/urls.py
@@ -4,11 +4,14 @@ from posts.views import *
 urlpatterns = [
     #Post관련
     #path('<int:id>', get_post_detail), # Post 내용 조회
-    path('', post_list, name="post_list"), #Post 전체 조회
-    path('<int:post_id>/', post_detail, name = 'post_detail'),#Post 단일 조회
-    path('comments/', get_comments_in_posts_detail), #해당 Post의 Comments_all 조회
+    #path('', post_list, name="post_list"), #Post 전체 조회
+    #path('<int:post_id>/', post_detail, name = 'post_detail'),#Post 단일 조회
+    #path('comments/', get_comments_in_posts_detail), #해당 Post의 Comments_all 조회
+    path('comments/', CommentList.as_view()),
+    path('', PostList.as_view()), # post 전체 조회
+    path('<int:post_id>/', PostDetail.as_view()), # post 개별 조회
 
     #Comment관련
-    path('comment/<int:comment_id>/',get_comment_detail),# Comment 단일 조회
-    
+    #path('comment/<int:comment_id>/',get_comment_detail),# Comment 단일 조회
+    path('comment/<int:comment_id>/',CommentDetail.as_view()),
 ]
